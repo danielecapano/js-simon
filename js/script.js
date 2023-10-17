c267fa8daf16c2afabd39ff45b98d20ff3600f86
danielecapano <daniele.capano@gmail.com>
2023-10-17 16:11:20 +0200

    ADD - basic code for generate random numbers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("simon");
+// console.log("simon");
+const numbersDomElement = document.querySelector(".numbers");
+const arrayRandomInt = [];
+
+do {
+  const n = Math.floor(Math.random() * (100 - 1) + 1);
+  arrayRandomInt.push(n);
+} while (arrayRandomInt.length < 5);
+
+console.log(arrayRandomInt);
+
+for (let i = 0; i < arrayRandomInt.length; i++) {
+  const number = arrayRandomInt[i];
+  const htmlContent = `<h1>${number}</h1>`;
+  numbersDomElement.innerHTML += htmlContent;
+}
+setTimeout(() => {
+  numbersDomElement.classList.add("hide");
+}, 5000);
