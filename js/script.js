ceb08dc4897588fcfcd5b20c155c1433ebfdfbf0
danielecapano <daniele.capano@gmail.com>
2023-10-18 14:29:48 +0200

    FIX - code for verify array

--- js/script.js
+++ js/script.js
@@ -1,52 +1,46 @@
-// console.log("simon");
 const numbersDomElement = document.querySelector(".numbers");
 const userResultsDomElement = document.querySelector(".user-results");
 const userNumbersDomElement = document.querySelector(".user-numbers");
 const scoreDomElement = document.querySelector(".score");
 const arrayRandomInt = [];
 
-let counter = 0;
-
 do {
   const n = Math.floor(Math.random() * (100 - 1) + 1);
-  arrayRandomInt.push(n);
+  if (!arrayRandomInt.includes(n)) {
+    arrayRandomInt.push(n);
+  }
 } while (arrayRandomInt.length < 5);
 
-console.log(arrayRandomInt);
-
-// for (let i = 0; i < arrayRandomInt.length; i++) {
-//   const number = arrayRandomInt[i];
-//   const htmlContent = `<h1>${number}</h1>`;
-//   numbersDomElement.innerHTML += htmlContent;
-// }
-
 displayNumberToDom(arrayRandomInt, numbersDomElement);
-setTimeout(() => {
-  setTimeout(verifiedUserNumbers(arrayRandomInt), 1000);
-}, 5000);
+// setTimeout(() => {
+//   hideNumbers();
+//   setTimeout(verifiedUserNumbers(arrayRandomInt), 1000);
+// }, 5000);
 
-console.log(counter);
+setTimeout(() => {
+  hideNumbers();
+}, 5000);
+setTimeout(() => {
+  verifiedUserNumbers(arrayRandomInt);
+}, 6000);
 
 function verifiedUserNumbers(array) {
   const arrayUserNumbers = [];
-
-  console.log(counter);
+  const arrayExactlyNumbers = [];
 
   do {
     const n = parseInt(prompt("Inserisci un numero della lista"));
     arrayUserNumbers.push(n);
-    if (array.includes(n) && !arrayUserNumbers.includes(n)) {
-      counter++;
+    if (array.includes(n) && !arrayExactlyNumbers.includes(n)) {
+      arrayExactlyNumbers.push(n);
     }
   } while (arrayUserNumbers.length < 5);
-  console.log(arrayUserNumbers, counter);
+
   numbersDomElement.classList.remove("hide");
 
   displayNumberToDom(arrayUserNumbers, userNumbersDomElement);
   userResultsDomElement.classList.remove("hide");
-  scoreDomElement.innerHTML = `Il tuo punteggio è ${counter}`;
-
-  return counter;
+  scoreDomElement.innerHTML = `Il tuo punteggio è ${arrayExactlyNumbers.length}`;
 }
 
 function displayNumberToDom(array, domElement) {
@@ -55,4 +49,8 @@ function displayNumberToDom(array, domElement) {
     const htmlContent = `<h1>${number}</h1>`;
     domElement.innerHTML += htmlContent;
   }
+}
+
+function hideNumbers() {
+  numbersDomElement.classList.add("hide");
 }
